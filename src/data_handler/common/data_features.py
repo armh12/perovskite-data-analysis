26916af3be518f0ddc3f9761d27c24aaa94c10b4
armh12 <hovhannisyanarman062@gmail.com>
2025-02-02 20:21:50 +0400

    finished feature creating

--- src/data_handler/common/data_features.py
+++ src/data_handler/common/data_features.py
@@ -1,13 +1,17 @@
 import ast
+import math
 import pandas as pd
+from pymatgen.core import Composition
 from pymatgen.core.lattice import Lattice
 from pymatgen.core.molecular_orbitals import MolecularOrbitals
-from pymatgen.core.sites import Site
+from pymatgen.core.periodic_table import Element
+
+from data_handler.common.entities import AM3_TO_SM3, AMU_TO_GRAM
 
 
-def filter_valid_perovkiste_by_name(df: pd.DataFrame) -> pd.DataFrame:
-    if "name" in df.columns:
-        df = df[df["name"].str.endswith("3")]
+def filter_valid_perovkiste_by_name(df: pd.DataFrame, field_name: str) -> pd.DataFrame:
+    if field_name in df.columns:
+        df = df[df[field_name].str.endswith("3")]
     return df
 
 
@@ -16,12 +20,70 @@ def parse_formula(formula: str) -> pd.Series:
     return molecular_orbitals.composition
 
 
+def classify_material(formula: str) -> str:
+    """
+    Classify a material as 'organic', 'inorganic', or 'semi-organic' based on its formula.
+
+    Heuristics:
+      - If the composition contains carbon (C) and hydrogen (H):
+           • And if it also contains any metal element (e.g. Pb, Cs, etc.), then it is considered "semi-organic" (a hybrid).
+           • If no metal is present, then it is likely "organic".
+      - If neither C nor H is present, it is considered "inorganic".
+
+    Note:
+      • This is a simple heuristic. For instance, some inorganic compounds like carbonates (containing C) are inorganic,
+        and the connectivity in a periodic structure might need more careful treatment.
+      • You can refine the list of metals or even check oxidation states for better accuracy.
+
+    Parameters:
+      formula (str): The reduced chemical formula (e.g., "CsPbI3", "CH3NH3PbI3", "C10H12N2O").
+
+    Returns:
+      str: One of "organic", "semi-organic", or "inorganic".
+        """
+    comp = Composition(formula)
+    elements = [str(el) for el in comp.elements]
+
+    metals = []
+    for el in elements:
+        try:
+            if Element(el).is_metal:
+                metals.append(el)
+        except Exception:
+            pass
+
+    contains_carbon = "C" in elements
+    contains_hydrogen = "H" in elements
+
+    if contains_carbon and contains_hydrogen:
+        if metals:
+            return "semi-organic"
+        else:
+            return "organic"
+    else:
+        return "inorganic"
+
+
+def get_composition_features(df: pd.DataFrame) -> pd.DataFrame:
+    def __parse_composition(formula: str) -> pd.Series:
+        composition = Composition(formula)
+        weight = composition.weight
+        avg_elec_negativity = composition.average_electroneg
+        scalar_series = pd.Series({
+            "weight": weight,
+            "avg_elec_negativity": avg_elec_negativity
+        })
+        return scalar_series
+
+    parsed_df = df["chemical_formula_reduced"].apply(__parse_composition)
+    df = pd.concat([df, parsed_df], axis=1)
+    return df
+
+
 def decompose_sites(phases_df: pd.DataFrame) -> pd.DataFrame:
     def __parse_site(site_str: str):
         element, coords = site_str.split('@')
         x, y, z = map(float, coords.strip().split())
-        sites = Site(element, ((x, y, z)))
-        print(sites)
         return element, x, y, z
 
     def __process_sites(row: pd.Series):
@@ -77,7 +139,7 @@ def split_element_names(phases_df: pd.DataFrame):
     return phases_df
 
 
-def parse_unit_cells(phases_df: pd.DataFrame) -> pd.DataFrame:
+def parse_lattice_vectors(structures_df: pd.DataFrame) -> pd.DataFrame:
     def __parse_unit_cell(unit_cell: list[list]) -> pd.Series:
         lattice = Lattice(unit_cell)
         lattice_dict = {
@@ -90,7 +152,43 @@ def parse_unit_cells(phases_df: pd.DataFrame) -> pd.DataFrame:
         }
         return pd.Series(lattice_dict)
 
-    unit_cell_parsed = phases_df["unit_cell"].apply(__parse_unit_cell)
-    phases_df = pd.concat([phases_df, unit_cell_parsed], axis=1)
-    phases_df.drop(columns=["unit_cell"], inplace=True)
+    unit_cell_parsed = structures_df["lattice_vectors"].apply(__parse_unit_cell)
+    structures_df = pd.concat([structures_df, unit_cell_parsed], axis=1)
+    structures_df.drop(columns=["lattice_vectors"], inplace=True)
+    return structures_df
+
+
+def add_density(structures_df: pd.DataFrame) -> pd.DataFrame:
+    def _calculate_density(row):
+        try:
+            lattice = Lattice.from_parameters(
+                row['a'], row['b'], row['c'],
+                row.get('alpha', 90), row.get('beta', 90), row.get('gamma', 90)
+            )
+            total_mass = sum(Element(el).atomic_mass for el in row.species_at_sites)
+            volume_cm3 = lattice.volume * AM3_TO_SM3
+            total_mass_g = total_mass * AMU_TO_GRAM
+            density = total_mass_g / volume_cm3
+            return density
+        except Exception:
+            return None
+
+    structures_df['density'] = structures_df.apply(_calculate_density, axis=1)
+    return structures_df
+
+
+def add_tolerance_factor(phases_df: pd.DataFrame) -> pd.DataFrame:
+    def _calculate_tolerance(row):
+        try:
+            r_A = Element(row['A_element']).average_cationic_radius
+            r_B = Element(row['B_element']).average_cationic_radius
+            r_X = Element(row['X_element']).average_ionic_radius
+            if r_A is None or r_B is None or r_X is None:
+                return None
+            tolerance = (r_A + r_X) / (math.sqrt(2) * (r_B + r_X))
+            return tolerance
+        except Exception:
+            return None
+
+    phases_df['tolerance_factor'] = phases_df.apply(_calculate_tolerance, axis=1)
     return phases_df
--- src/data_handler/oqmd/perovskite_data.py
+++ src/data_handler/oqmd/perovskite_data.py
@@ -1,14 +1,15 @@
 import pandas as pd
 
 from data_handler.common.data_features import decompose_sites, split_element_names, filter_valid_perovkiste_by_name, \
-    parse_formula, parse_unit_cells
+    parse_formula, parse_lattice_vectors, add_density, add_tolerance_factor, get_composition_features, classify_material
 from data_handler.oqmd.client import OQMDAsyncClient
 
 
 class PerovskiteDataHandler:
     PEROVSKITE_FILTER = {"generic": "ABC3"}
-    phases_fields = ["name", "entry_id", "formationenergy_id", "spacegroup", "volume", "ntypes", "natoms", "band_gap",
-                     "delta_e", "stability", "unit_cell", "sites"]
+    phases_fields = ["name", "entry_id", "spacegroup", "volume", "ntypes", "natoms", "band_gap",
+                     "delta_e", "stability", "sites"]
+    structures_fields = ["chemical_formula_reduced", "_oqmd_entry_id", "lattice_vectors", "species_at_sites"]
 
     def __init__(self,
                  client: OQMDAsyncClient, ):
@@ -17,22 +18,31 @@ class PerovskiteDataHandler:
     async def get_phases(self, max_pages: int | None = None) -> pd.DataFrame:
         json_response = await self.client.get_phases(self.phases_fields, self.PEROVSKITE_FILTER, max_pages)
         df = pd.DataFrame(json_response)
-        df = filter_valid_perovkiste_by_name(df)
+        df = filter_valid_perovkiste_by_name(df, "name")
         df["_composition"] = df["name"].apply(parse_formula)
         df = decompose_sites(df)
         df = split_element_names(df)
-        df = parse_unit_cells(df)
+        df = add_tolerance_factor(df)
+        df["classification"] = df["name"].apply(classify_material)
         df.drop(columns=["_composition"], inplace=True)
-        df.rename(columns={"delta_e": "e_hull", "entry_id": "id"})
+        df.rename(columns={"delta_e": "e_hull", "entry_id": "id"}, inplace=True)
         return df
 
-    async def get_structures(self, offset: int | None = None, max_pages: int | None = None) -> pd.DataFrame:
+    async def get_structures(self, max_pages: int | None = None) -> pd.DataFrame:
         json_response = await self.client.get_structures(self.PEROVSKITE_FILTER, max_pages)
         attrs = [_json_response["attributes"] for _json_response in json_response]
         df = pd.DataFrame(attrs)
+        df = df[self.structures_fields]
+        df = filter_valid_perovkiste_by_name(df, "chemical_formula_reduced")
+        df["_composition"] = df["chemical_formula_reduced"].apply(parse_formula)
+        df = parse_lattice_vectors(df)
+        df = add_density(df)
+        df = get_composition_features(df)
+        df.rename(columns={"_oqmd_entry_id": "id"}, inplace=True)
+        df.drop(columns=["_composition", "species_at_sites"], inplace=True)
         return df
 
-    async def get_calculations(self, offset: int | None = None, max_pages: int | None = None) -> pd.DataFrame:
+    async def get_calculations(self, max_pages: int | None = None) -> pd.DataFrame:
         json_response = await self.client.get_calculations(None, self.PEROVSKITE_FILTER, max_pages)
         df = pd.DataFrame(json_response)
         return df
